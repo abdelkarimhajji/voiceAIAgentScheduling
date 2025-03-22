10d9ecf5374311c6e563a931b87d1df42ee930f4
abdelkarimhajji <abdelkarim.hujji@gmail.com>
2025-03-22 01:59:23 +0000

    i add the twilio and openai api and also api of calendar

--- src/authRunner.ts
+++ src/authRunner.ts
@@ -1,11 +1,11 @@
 import { authorize } from './googleAuth';
-
+   
 (async () => {
   try {
     await authorize();
     console.log('gogle calendar authorization complete! ');
   } catch (error) {
-    
+
     console.error('authorization failed :', error);
   }
 })();
